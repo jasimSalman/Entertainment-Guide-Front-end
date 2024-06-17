28a30b9eaf37119c9ba0510a3ade3773df75a006
m00hammed <m00hammed776@gmail.com>
2024-06-17 21:55:10 +0300

    I fixed the style of inputs & main page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 const About = () => {
   return (
-    <div>
+    <div className="about">
       <h1>Who are we?</h1>
       <p>
       Lorem Ipsum is simply dummy text of theprinting and typesetting industry. Lorem Ipsumhas been the industry's standard dummy textever since the 1500s, when an unknown printertook a galley of type and scrambled it to makea type specimen book. It has survived not onlyfive centuries, but also the leap into electronictypesetting, remaining essentially unchanged. Itwas popularised in the 1960s with the release
