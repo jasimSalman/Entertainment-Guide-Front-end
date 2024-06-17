498c8d995f77a4d6245895d2104d7ee50a04123b
jasimSalman <jassimmo7amed+1@gmail.com>
2024-06-17 16:23:36 +0300

    craete booking page to show all the user bookings

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,9 +1,36 @@
-import "../App.css"
+import '../App.css'
+import { useState, useEffect } from 'react'
+import Client from '../services/api'
+import BookingCard from '../components/BookingCard'
 
-const Bookings = () => {
+const Bookings = ({ userId }) => {
+  const [bookings, setBookings] = useState([])
+
+  useEffect(() => {
+    const getBookings = async () => {
+      try {
+        const res = await Client.get(`http://localhost:3001/book/${userId}`)
+        console.log(res.data)
+        setBookings(res.data)
+      } catch (err) {
+        console.log('Error fetching bookingd:', err)
+      }
+    }
+    getBookings()
+  }, [userId])
+
   return (
     <div className="Bookings">
-      <h1>Bookings Page</h1>
+      <ul>
+        {bookings.map((book) => (
+          <BookingCard
+            name={book.place.placeName}
+            start={book.start}
+            end={book.end}
+            key={book.id}
+          />
+        ))}
+      </ul>
     </div>
   )
 }
