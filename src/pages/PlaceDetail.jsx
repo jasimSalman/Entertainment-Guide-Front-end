3d011a99b8d3cf7921560996dc11a61d9ab9f18d
jasimSalman <jassimmo7amed+1@gmail.com>
2024-06-18 18:46:39 +0300

    edit add place page

--- src/pages/PlaceDetail.jsx
+++ src/pages/PlaceDetail.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import axios from 'axios'
 import PlaceDetailsCard from '../components/PlaceDetailsCard'
 import FavList from '../components/FavList'
@@ -7,15 +7,28 @@ import Review from '../components/Review'
 import Client from '../services/api'
 
 const placeDetails = () => {
+  let navigate = useNavigate()
   const [PlaceDetails, setPlaceDetails] = useState({})
   const [reviews, setReviews] = useState([])
 
   const userId = localStorage.getItem('userId')
 
   let { placeId } = useParams()
+  console.log(`Place Id ${placeId}`)
+  console.log(`User Id ${userId}`)
 
-  const deletPlace = async () => {
-    await Client.delete(`/places/${placeId}/${userId}`)
+  const deletePlace = async () => {
+    try {
+      const response = await Client.delete(`/places/${placeId}/${userId}`)
+      if (response.status === 200 || response.status === 204) {
+        console.log('Place deleted successfully')
+        navigate('/myPlaces')
+      } else {
+        console.error('Failed to delete place:', response.status)
+      }
+    } catch (error) {
+      console.error('Failed to delete place:', error)
+    }
   }
 
   useEffect(() => {
@@ -46,7 +59,8 @@ const placeDetails = () => {
         placeDescription={PlaceDetails.placeDescription}
         placeLocation={PlaceDetails.placeLocation}
       />
-      <button onClick={deletPlace}>Delete Place</button>
+      {userId && <button onClick={deletePlace}>Delete Place</button>}
+
       <Review reviews={reviews} placeId={placeId} />
       <FavList placeId={placeId} />
     </div>
