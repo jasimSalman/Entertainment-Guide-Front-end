58e4b688ca5098828f03bd2d311125e9ac477820
jasimSalman <jassimmo7amed+1@gmail.com>
2024-06-18 20:23:05 +0300

    modify the update passowrd page

--- src/services/Auth.js
+++ src/services/Auth.js
@@ -1,11 +1,11 @@
-import Client from "./api"
+import Client from './api'
 
 export const SignInUser = async (data) => {
   try {
-    const res = await Client.post("/auth/login", data)
-    localStorage.setItem("token", res.data.token)
-    localStorage.setItem("userId", res.data.user.id)
-    localStorage.setItem("userType", res.data.user.type)
+    const res = await Client.post('/auth/login', data)
+    localStorage.setItem('token', res.data.token)
+    localStorage.setItem('userId', res.data.user.id)
+    localStorage.setItem('userType', res.data.user.type)
     return res.data.user
   } catch (error) {
     throw error
@@ -14,7 +14,7 @@ export const SignInUser = async (data) => {
 
 export const RegisterUser = async ({ email, password }) => {
   try {
-    const res = await Client.post("/auth/register", { email, password })
+    const res = await Client.post('/auth/register', { email, password })
     return res.data
   } catch (error) {
     throw error
@@ -23,7 +23,7 @@ export const RegisterUser = async ({ email, password }) => {
 
 export const updatePassword = async (data) => {
   try {
-    const res = await Client.put("/auth/updatePassword", data)
+    const res = await Client.put('/auth/reset-password', data)
     return res.data
   } catch (error) {
     throw error
@@ -32,7 +32,7 @@ export const updatePassword = async (data) => {
 
 export const CheckSession = async () => {
   try {
-    const res = await Client.get("/auth/session")
+    const res = await Client.get('/auth/session')
     return res.data
   } catch (error) {
     throw error
