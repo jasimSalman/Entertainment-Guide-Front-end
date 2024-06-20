893a295442103ad112f361e2dc563139723110dc
m00hammed <m00hammed776@gmail.com>
2024-06-20 14:23:07 +0300

    I create a new component to contain the photos in main page & I style multible page in the app

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 // import "../App.css"
 import About from '../components/About'
+import MainPhoto from '../components/MainPhoto'
 
 const Home = () => {
   return (
     <div className="home">
+      <MainPhoto/>
       <About />
     </div>
   )
