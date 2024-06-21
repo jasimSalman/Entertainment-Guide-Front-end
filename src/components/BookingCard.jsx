33d8f4abc3ea975888c7a825d5e62cf416a80408
m00hammed <m00hammed776@gmail.com>
2024-06-21 17:09:41 +0300

    I create component for the photoes of home page & I gave them transaction feature

--- src/components/BookingCard.jsx
+++ src/components/BookingCard.jsx
@@ -1,6 +1,6 @@
 const BookingCard = ({ name, start, end, key }) => {
   return (
-    <li key={key}>
+    <li className="booking-card" key={key}>
       <div>{name}</div>
       <div>Start Time: {start}</div>
       <div>End Time: {end}</div>
--- src/components/MainPhoto.jsx
+++ src/components/MainPhoto.jsx
@@ -6,10 +6,13 @@ import '../App.css'
 const MainPhoto = () => {
   return (
     <div className='main-photo'>
-        <img src='https://img.freepik.com/free-photo/view-3d-cinema-theatre-room_23-2151067056.jpg' width="100px" height="100px" />
-        <img src='https://img.freepik.com/free-photo/view-3d-cinema-theatre-room_23-2151067056.jpg' width="100px" height="100px" />
-        <img src='https://img.freepik.com/free-photo/view-3d-cinema-theatre-room_23-2151067056.jpg' width="100px" height="100px" />
-        <img src='https://img.freepik.com/free-photo/view-3d-cinema-theatre-room_23-2151067056.jpg' width="100px" height="100px" />
+        <img src='https://t3.ftcdn.net/jpg/06/34/93/50/360_F_634935028_Y5sy10kpKnbMPRD0huZi1tDodoKqsWWY.jpg' width="100px" height="100px" />
+             <img src='https://t3.ftcdn.net/jpg/06/08/30/46/360_F_608304605_XtA3ZsapU7trdIOyUeTCgH4Snh3AX0aS.jpg' width="100px" height="100px" />
+        <img src='https://t4.ftcdn.net/jpg/08/42/17/95/360_F_842179504_VLjMfoH1Rz7DJZiulXENstxCK2TYIBmE.jpg' width="100px" height="100px" />
+        <img src='https://img.freepik.com/free-photo/view-3d-cinema-theatre-room_23-2151067056.jpg' width="100px" height="100px" />  
+        <img src='https://c1.wallpaperflare.com/preview/653/855/808/aquarium-photos-hungary-marine.jpg' width="100px" height="100px" /> 
+
+
     </div>
   )
 }
